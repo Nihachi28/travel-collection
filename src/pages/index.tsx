5114ef6ec92471d17af86aacdf42c8c04ef5bdc4
杨逸钧 <yang.yijun@opg.cn>
2024-05-16 10:00:31 +0800

    fix bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-import PhotoCard from "@/components/PhotoCard";
 import Head from "next/head";
 import Banner from "@/components/Banner";
 import { Button } from "@/components/ui/button";
@@ -13,34 +12,12 @@ const Home: React.FC = () => {
         { src: '/IMG_2020.JPG', alt: 'Cityscape', description: 'A vibrant cityscape at night.' },
     ];
 
-    // return (
-    //     <div className="min-h-screen bg-gray-100">
-    //         <div className="front">
-    //             <div className="front-content">
-    //                 <h1 className="text-4xl font-bold">Travel Gallery</h1>
-    //                 <p className="mt-2">By Eugene Yeung</p>
-    //             </div>
-    //         </div>
-    //         <div className="container mx-auto py-8 px-4">
-    //             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-    //                 {photos.map((photo, index) => (
-    //                     <PhotoCard key={index} src={photo.src} alt={photo.alt} description={photo.description} />
-    //                 ))}
-    //             </div>
-    //         </div>
-    //     </div>
-    // );
-
     return (
         <Layout>
             <div className="h-8"></div>
             <div className="relative w-full">
                 <div className="full-bg h-16"/>
             </div>
-            {/* <div className="container mx-auto p-4">
-                <h1 className="text-3xl font-bold">Website Title</h1>
-                <p className="mt-4">Author Name</p>
-            </div> */}
         </Layout>
     );
 }
